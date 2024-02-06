77b661569ede863908e8914b774c44dab0b4a14a
alen-omarbayev <alen.omarbayev2005@gmail.com>
2024-02-06 21:27:59 +0600

    functon 1 ex3

---
+++ lab3/func1_3.py
@@ -0,0 +1,11 @@
+def solve(numheads, numlegs):
+    for num_chickens in range(numheads + 1):
+        num_rabbits = numheads - num_chickens
+        if (2 * num_chickens + 4 * num_rabbits) == numlegs:
+            return num_chickens, num_rabbits
+    return None
+
+
+numheads = 35
+numlegs = 94
+print(solve(numheads, numlegs))
