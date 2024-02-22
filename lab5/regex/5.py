e886863b990042d00ea777823d43a2875498de40
alen-omarbayev <alen.omarbayev2005@gmail.com>
2024-02-22 16:50:49 +0600

    regex5

---
+++ lab5/regex/5.py
@@ -0,0 +1,10 @@
+import re
+
+file_path = r"C:\Users\ASUS\OneDrive\Рабочий стол\pp2\pp2\lab5\regex\row.txt"
+
+with open(file_path, 'r', encoding='utf-8') as file:
+    rows = file.readlines()
+
+for row in rows:
+    if re.findall("a[a-zA-z]b$", row):
+        print(row.strip())
