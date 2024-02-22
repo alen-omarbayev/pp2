ff88c7b0013b58d6eff796e05129eb83e242b14b
alen-omarbayev <alen.omarbayev2005@gmail.com>
2024-02-22 16:30:54 +0600

    regex3

--- lab5/regex/3.py
+++ lab5/regex/3.py
@@ -1,10 +1,10 @@
 import re
 
-file_path = r"C:\Users\ASUS\OneDrive\Рабочий стол\pp2\pp2\lab4\regex\row.txt"
+file_path = r"C:\Users\ASUS\OneDrive\Рабочий стол\pp2\pp2\lab5\regex\row.txt"
 
 with open(file_path, 'r', encoding='utf-8') as file:
     rows = file.readlines()
 
 for row in rows:
-    if re.findall("[a-z]+_+[a-z]|_+[a-z]+_+[a-z]|[a-z]+_+[a-z]+_|_+[a-z]+_+[a-z]+_|_+[a-z]|[a-z]+_|_+[a-z]+_|", row):
+    if re.findall("[a-z]+_[a-z]+_", row):
         print(row.strip())
