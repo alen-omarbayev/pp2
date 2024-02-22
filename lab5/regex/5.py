6aafe9cce8a70dd66faf11510a8eec3ef7bfd91c
alen-omarbayev <alen.omarbayev2005@gmail.com>
2024-02-22 16:52:28 +0600

    regex5

--- lab5/regex/5.py
+++ lab5/regex/5.py
@@ -6,5 +6,5 @@ with open(file_path, 'r', encoding='utf-8') as file:
     rows = file.readlines()
 
 for row in rows:
-    if re.findall("a[a-zA-z]b$", row):
+    if re.findall("a[a-zA-Z]b$", row):
         print(row.strip())
