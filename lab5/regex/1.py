d4c04444a0d7a7aa9acdd7dbbe4b49af2ae057f2
alen-omarbayev <alen.omarbayev2005@gmail.com>
2024-02-22 16:23:50 +0600

    lab5

---
+++ lab5/regex/1.py
@@ -0,0 +1,10 @@
+import re
+
+file_path = r"C:\Users\ASUS\OneDrive\Рабочий стол\pp2\pp2\lab4\regex\row.txt"
+
+with open(file_path, 'r', encoding='utf-8') as file:
+    rows = file.readlines()
+
+for row in rows:
+    if re.findall("a.*b", row):
+        print(row.strip())
