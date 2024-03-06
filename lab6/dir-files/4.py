af35a4223b77a540e2d4608a799d495a266aac19
alen-omarbayev <alen.omarbayev2005@gmail.com>
2024-03-06 22:44:16 +0500

    dir4

--- lab6/dir-files/4.py
+++ lab6/dir-files/4.py
@@ -1,6 +1,6 @@
 import os
 
-path = r"C:\\Users\\ASUS\\OneDrive\\Рабочий стол\\pp2\\pp2\\lab6\\for_dirs"
+path = r"C:\\Users\\ASUS\\OneDrive\\Рабочий стол\\pp2\\pp2\\lab6\\for_dirs\dwada.txt"
 with open(path, 'r') as file:
     line_count = sum(1 for line in file)
 
