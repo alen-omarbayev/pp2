503c9df5ae9767b57499b5c9a493b1b47451232b
alen-omarbayev <alen.omarbayev2005@gmail.com>
2024-03-06 23:43:03 +0500

    dir6

---
+++ lab6/dir-files/6.py
@@ -0,0 +1,4 @@
+import string
+for char in string.ascii_uppercase:
+    file = open('C:\\Users\\ASUS\\OneDrive\\Рабочий стол\\pp2\\pp2\\lab6\\for_dirs\{fchar}'.format(fchar = char), 'x')
+    file.close()
