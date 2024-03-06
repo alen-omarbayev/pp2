cfd6e1fcd233d4f96ed415c8a017ea23c7b83ece
alen-omarbayev <alen.omarbayev2005@gmail.com>
2024-03-06 22:41:01 +0500

    dir4

---
+++ lab6/dir-files/4.py
@@ -0,0 +1,7 @@
+import os
+
+path = r"C:\\Users\\ASUS\\OneDrive\\Рабочий стол\\pp2\\pp2\\lab6\\for_dirs"
+with open(path, 'r') as file:
+    line_count = sum(1 for line in file)
+
+print(f'number of lines in a file: {line_count}') 
