3077971002ecbccd93b5025a6763a17b833e5f2a
alen-omarbayev <alen.omarbayev2005@gmail.com>
2024-03-06 21:36:21 +0500

    dirs1

--- lab6/dir-files/1.py
+++ lab6/dir-files/1.py
@@ -2,19 +2,16 @@ import os
 
 path = r"C:\Users\ASUS\OneDrive\Рабочий стол\pp2\pp2\lab6\for_dirs"
 
-# List only directories
 print("Directories:")
 for entry in os.scandir(path):
     if entry.is_dir():
         print(entry.name)
 
-# List only files
 print("\nFiles:")
 for entry in os.scandir(path):
     if entry.is_file():
         print(entry.name)
 
-# List all directories and files
 print("\nAll Directories and Files:")
 for entry in os.scandir(path):
     print(entry.name)
