a437ab1091bada66fb29f2965e8fd8a81a1da3a8
alen-omarbayev <alen.omarbayev2005@gmail.com>
2024-03-27 02:01:29 +0500

    images

--- lab7/1.py
+++ lab7/1.py
@@ -1,29 +1,57 @@
-import pygame
+import pygame 
+import time
+import datetime
+import math
 
 pygame.init()
+RES = WIDTH ,HEIGHT = 1400,1050
+middle = WIDTH//2 , HEIGHT//2
+RADIUS = 1000
 
-done = False
+screen = pygame.display.set_mode((RES))
+clock = pygame.time.Clock()
 
-X = 600
-Y = 600
- 
-# create the display surface object
-# of specific dimension..e(X, Y).
-scrn = pygame.display.set_mode((X, Y))
- 
-# set the pygame window name
-pygame.display.set_caption('image')
- 
-# create a surface object, image is drawn on it.
-imp = pygame.image.load("C:\\Users\\DELL\\Downloads\\gfg.png").convert()
- 
-# Using blit to copy content from one surface to other
-scrn.blit(imp, (0, 0))
+pygame.display.set_caption("Mickey Clock")
 
-while not done:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        done = True
-        
-        pygame.display.flip()
+sec = pygame.image.load(r"C:\Users\ASUS\OneDrive\Рабочий стол\pp2\pp2\lab7\leftarm.png").convert_alpha()
+minute = pygame.image.load(r"C:\Users\ASUS\OneDrive\Рабочий стол\pp2\pp2\lab7\rightarm.png").convert_alpha()
+rectsec = sec.get_rect()
+rectmin = minute.get_rect()
+rectmin.center = middle
 
+background = pygame.image.load(r"C:\Users\ASUS\OneDrive\Рабочий стол\pp2\pp2\lab7\mainclock.png")
+run = True
+
+angle1 = 0
+angle2 = 0
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    #system time
+    time = datetime.datetime.now()
+    minuteTime = time.minute
+    secondTime = time.second
+
+    #minute
+    angle1 = -minuteTime*6 #6 is degree
+    leg1 = pygame.transform.rotate(minute, angle1)
+    rect1 = leg1.get_rect()
+    rect1.center = rectmin.center
+
+    #second
+    angle2 = -secondTime*6 #6 is degree
+    leg2 = pygame.transform.rotate(sec, angle2)
+    rect2 = leg2.get_rect()
+    rect2.center = rectsec.center
+    
+    #output
+    screen.blit(background, (0, 0))
+    screen.blit(leg1, rect1)
+    screen.blit(leg2, rect2)
+
+
+    #screen.blit(background, (0, 0))
+    #pygame.draw.circle(screen, (0, 0, 0), (500, 500), 490, 5)
+    pygame.display.flip()
+    clock.tick(60)
